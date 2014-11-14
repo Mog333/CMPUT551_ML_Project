c5c3a71fc973c0f3bfd213d85ab59567834f6d05
mog333 <robert.ag.post@gmail.com>
2014-11-13 17:06:04 -0700

    added score histogram and stem frequency reduction test to data analysis

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,10 +1,49 @@
 #! /usr/bin/env python
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
 import processTweets as pt
+from nltk.stem.lancaster import LancasterStemmer
+
+
+def plotScoreHistogram():
+   scores = pt.getTweetScoresFromFile("score.txt")
+   plt.hist(scores)
+   plt.title("Score Histogram")
+   plt.xlabel("Value")
+   plt.ylabel("Frequency")
+   plt.show()
+
+def stemReductionTest():
+   st = LancasterStemmer()   
+   tweets = pt.getTweetsFromFile("tweet.txt")
+   stemCount = 0
+   
+   bow_dict = {}
+   stemmed_dict = {}
+   
+   for tweet in tweets:
+      for word in tweet.split():
+         if(word[0] == '@'):
+            continue
+         
+         try:
+            stemmedWord = st.stem(word)
+         except UnicodeDecodeError:
+            stemmedWord = word
+
+         if stemmedWord in stemmed_dict:
+            stemmed_dict[stemmedWord]['count'] += 1
+         else:
+            stemmed_dict[stemmedWord] = {'index':len(stemmed_dict), 'count':1}
+
+   bow_dict = pt.addNGramToDict(tweets, bow_dict, 1)
+
+   print("unigram dict length: " + str(len(bow_dict)) + " stemmed dict length: " + str(len(stemmed_dict)))
+   return stemmed_dict
+   
+
 
 def getFreqDict(bow_dict):
    #returns a dictionary with keys being the counts of words in a bow dict, and values being the number of words with that count.
@@ -32,3 +71,4 @@ def plotUnigramFrequency():
    plt.show()
 
    return freqDict
+
