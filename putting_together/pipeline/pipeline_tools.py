752384830e5f1521922b4dec5e6882a2af4edefd
Janek <janek.goergens@gmail.com>
2014-11-24 15:21:23 -0700

    Changed choices array indexing

--- putting_together/pipeline/pipeline_tools.py
+++ putting_together/pipeline/pipeline_tools.py
@@ -3,104 +3,92 @@ import os
 from ConfigParser import SafeConfigParser
 
 def buildChoiceArray():
-	choices = []
+	choices = {}
 
+	###
+	# Global
+	###
+	choices['global'] = {}
+	choices['global']['text'] = ''
+	choices['global']['pos_values'] = ''
+	choices['global']['value'] = -1
+	choices['global']['subs'] = ['num_examples','preprocessing','cross_val']
 
 	###
 	# Number of Examples
 	###
-	tmp = {}
-	tmp['name'] = 'num_examples'
-	tmp['text'] = '#Examples'
-	tmp['pos_values'] = {0: '50', 1: '100', 2: '500', 3: '1500', 4: '8000'}
-	tmp['value'] = -1
-	tmp['subs'] = []
-	choices.append(tmp)
+	choices['num_examples'] = {}
+	choices['num_examples']['text'] = '#Examples'
+	choices['num_examples']['pos_values'] = 'int'
+	choices['num_examples']['value'] = -1
+	choices['num_examples']['subs'] = []
 
 	###
 	# Preprocessing
 	###
-	tmp = {}
-	tmp['name'] = 'preprocessing'
-	tmp['text'] = 'Perform preprocessing?'
-	tmp['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp['value'] = -1
-	tmp['subs'] = []
+	choices['preprocessing'] = {}
+	choices['preprocessing']['text'] = 'Perform preprocessing?'
+	choices['preprocessing']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['preprocessing']['value'] = -1
+	choices['preprocessing']['subs'] = ['pre_lower_chars','pre_appostrophy','pre_repetitions','pre_normalize']
 
 	# lower characters
-	tmp2 = {}
-	tmp2['name'] = 'lower_chars'
-	tmp2['text'] = 'Lower all characters?'
-	tmp2['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
+	choices['pre_lower_chars'] = {}
+	choices['pre_lower_chars']['text'] = 'Lower all characters?'
+	choices['pre_lower_chars']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['pre_lower_chars']['value'] = -1
+	choices['pre_lower_chars']['subs'] = []
 
 	# normalize words which contain appostrophy
-	tmp2 = {}
-	tmp2['name'] = 'appostrophy'
-	tmp2['text'] = 'Normalize words which contain appostrophy?'
-	tmp2['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
+	choices['pre_appostrophy'] = {}
+	choices['pre_appostrophy']['text'] = 'Normalize words which contain appostrophy?'
+	choices['pre_appostrophy']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['pre_appostrophy']['value'] = -1
+	choices['pre_appostrophy']['subs'] = []
 
 	# remove character repetitions. 
-	tmp2 = {}
-	tmp2['name'] = 'repetitions'
-	tmp2['text'] = 'Remove character repetitions?'
-	tmp2['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
+	choices['pre_repetitions'] = {}
+	choices['pre_repetitions']['text'] = 'Remove character repetitions?'
+	choices['pre_repetitions']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['pre_repetitions']['value'] = -1
+	choices['pre_repetitions']['subs'] = []
 
 	# pre-normilize. (imported code) external code, handles some case
 	# According to the source of the code:
 	# "#Twitter text comes HTML-escaped, so unescape it.
 	# We also first unescape &amp;'s, in case the text has been buggily double-escaped."
-	tmp2 = {}
-	tmp2['name'] = 'pre_normilize'
-	tmp2['text'] = 'Pre-normilize (unescape HTML)?'
-	tmp2['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
-
-	choices.append(tmp)
+	choices['pre_normalize'] = {}
+	choices['pre_normalize']['text'] = 'Pre-normilize (unescape HTML)?'
+	choices['pre_normalize']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['pre_normalize']['value'] = -1
+	choices['pre_normalize']['subs'] = []
 
 	###
 	# Cross Validation
 	###
-	tmp = {}
-	tmp['name'] = 'cross_val'
-	tmp['text'] = 'Perform Cross Validation?'
-	tmp['pos_values'] = {0: 'No', 1: 'Yes'}
-	tmp['value'] = -1
-	tmp['subs'] = []
+	choices['cross_val'] = {}
+	choices['cross_val']['text'] = 'Perform Cross Validation?'
+	choices['cross_val']['pos_values'] = {0: 'No', 1: 'Yes'}
+	choices['cross_val']['value'] = -1
+	choices['cross_val']['subs'] = ['cross_num_DataPoints','cross_num_folds']
 
 	# Number of DataPoints
-	tmp2 = {}
-	tmp2['name'] = 'num_DataPoints'
-	tmp2['text'] = '#DataPoints.'
-	tmp2['pos_values'] = 'int'
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
+	choices['cross_num_DataPoints'] = {}
+	choices['cross_num_DataPoints']['text'] = '#DataPoints.'
+	choices['cross_num_DataPoints']['pos_values'] = 'int'
+	choices['cross_num_DataPoints']['value'] = -1
+	choices['cross_num_DataPoints']['subs'] = []
 
 	# Number of Folds
-	tmp2 = {}
-	tmp2['name'] = 'Num_folds'
-	tmp2['text'] = '#Number of Folds.'
-	tmp2['pos_values'] = 'int'
-	tmp2['value'] = -1
-	tmp2['subs'] = []
-	tmp['subs'].append(tmp2)
-
-	choices.append(tmp)
+	choices['cross_num_folds'] = {}
+	choices['cross_num_folds']['text'] = '#Number of Folds.'
+	choices['cross_num_folds']['pos_values'] = 'int'
+	choices['cross_num_folds']['value'] = -1
+	choices['cross_num_folds']['subs'] = []
 
 	return choices
 
-def ask(choices, num = '', parser = ''):
+def ask(choices, questions = '', num = '', parser = ''):
 	if(num == ''):
 		if(len(sys.argv) >= 2):
 			if not os.path.exists(sys.argv[1]):
@@ -110,31 +98,33 @@ def ask(choices, num = '', parser = ''):
 			parser.read('simple.ini')
 		else:
 			parser = ''
-	# print type(parser)
-	# print not isinstance(parser, str)
+	
+	if(questions == ''):
+		questions = choices['global']['subs']
+
 	num2 = 1
-	for choice in choices:
-		if( isinstance(choice['pos_values'], str) ):
-			pos_values = choice['pos_values']
+	for question in questions:
+		if( isinstance(choices[question]['pos_values'], str) ):
+			pos_values = choices[question]['pos_values']
 		else:
 			pos_values = ''
-			for v in choice['pos_values']:
+			for v in choices[question]['pos_values']:
 				# print choice['pos_values'][v]
-				pos_values += '[%s: %s]' %(v, choice['pos_values'][v])
+				pos_values += '[%s: %s]' %(v, choices[question]['pos_values'][v])
 
 		try_ini = 1
 		while True:
-			print '%s%d) %s (%s)' %(num, num2, choice['text'], pos_values),
+			print '%s%d) %s (%s)' %(num, num2, choices[question]['text'], pos_values),
 			
-			if( not isinstance(parser, str) and parser.has_option('choices', choice['name']) and try_ini): # get from ini
-				v = parser.get('choices', choice['name'])
+			if( not isinstance(parser, str) and parser.has_option('choices', question) and try_ini): # get from ini
+				v = parser.get('choices', question)
 				print '%s (from ini)' % v
 				try_ini = 0
 			else: # get from user
 				v = raw_input()
 
-			if( isinstance(choice['pos_values'], str) ):
-				if(choice['pos_values'] == 'int'):
+			if( isinstance(choices[question]['pos_values'], str) ):
+				if(choices[question]['pos_values'] == 'int'):
 					try:
 						int(v)
 						break
@@ -142,7 +132,7 @@ def ask(choices, num = '', parser = ''):
 						pass
 			else:
 				try:
-				  choice['pos_values'][int(v)]
+				  choices[question]['pos_values'][int(v)]
 				  # print "you entered", choice['pos_values'][int(v)]
 				  break
 				except KeyError:
@@ -150,13 +140,13 @@ def ask(choices, num = '', parser = ''):
 				except ValueError:
 					pass
 
-		choice['value'] = int(v)
+		choices[question]['value'] = int(v)
 
 		
 		# print x
 
-		if(len(choice['subs']) and choice['value'] == 1):
-			choice['subs'] = ask(choice['subs'], '  %s%d.' % (num,num2), parser)
+		if(len(choices[question]['subs']) and choices[question]['value'] == 1):
+			choices[question]['subs'] = ask(choices, choices[question]['subs'], '  %s%d.' % (num,num2), parser)
 		num2 += 1
 
 	return choices
