b5b8d9e15fb44d4bfdd777ee110703a912c51515
Sankalp-UofA <sankalp@ualberta.ca>
2014-12-02 19:25:09 -0700

    tagged_tweets has all tweets with tags, also pushed the updated code to remove new line characters

--- POS Tagger Executable/PostProcessTaggedTweets.py
+++ POS Tagger Executable/PostProcessTaggedTweets.py
@@ -5,73 +5,75 @@ import os
 temp = os.system('./runTagger.sh --output-format conll tweet.txt >out.txt')
 
 f = open("out.txt",'r')
-f1 = open("postTagged.txt",'w+')
-f2 = open("tagged_tweets.txt",'w+')
+f2 = open("tagged_tweets.txt",'w')
+f3 = open("tweet.txt", 'r')
 
-array = [];
+lines = f3.readlines()
+
+tags = []
+tweet = []
+count = 0
 
 for line in f:
 	if(line=='\n'):
-		f1.write('\n')
+		tweet = lines[count]
+		tweet = tweet.split(" ")
+		tweet[-1] = tweet[-1].strip()
+		tweet = (" ").join(tweet + tags)
+		count+=1
+		f2.write(tweet)
 		f2.write('\n')
-		continue
+		tags = []
+		tweet = []
+		continue	
 	line = line.replace('\n','')
 	line = line.split('\t')
-	#print(line[0])
-	#print(line[1])
-	#print(line[2])
-	#print('\n')	
 	if(float(line[2])>.9 and line[1] in ('O','N','^','V','A','R','D','P','#','@','~','U','E',',','!')):
-		#print(line)
 		if(line[1]=='O'):
-			f2.write('*&O ')
+			#f2.write('*&O ')
+			tags.append('*&O')
 		if(line[1]=='N'):
-			f2.write('*&N ')
+			#f2.write('*&N ')
+			tags.append('*&N')
 		if(line[1]=='^'):
-			f2.write('*&^ ')
+			#f2.write('*&^ ')
+			tags.append('*&^')
 		if(line[1]=='V'):
-			f2.write('*&V ')
+			#f2.write('*&V ')
+			tags.append('*&V')
 		if(line[1]=='A'):
-			f2.write('*&A ')
+			#f2.write('*&A ')
+			tags.append('*&A')
 		if(line[1]=='R'):
-			f2.write('*&R ')
+			#f2.write('*&R ')
+			tags.append('*&R')
 		if(line[1]=='D'):
-			f2.write('*&D ')
+			#f2.write('*&D ')
+			tags.append('*&D')
 		if(line[1]=='P'):
-			f2.write('*&P ')
+			#f2.write('*&P ')
+			tags.append('*&P')
 		if(line[1]=='#'):
-			f2.write('*&# ')
+			#f2.write('*&# ')
+			tags.append('*&#')
 		if(line[1]=='@'):
-			f2.write('*&@ ')
+			#f2.write('*&@ ')
+			tags.append('*&@')
 		if(line[1]=='~'):
-			f2.write('*&~ ')
+			#f2.write('*&~ ')
+			tags.append('*&~')
 		if(line[1]=='U'):
-			f2.write('*&U ')
+			#f2.write('*&U ')
+			tags.append('*&U')
 		if(line[1]=='E'):
-			f2.write('*&E ')
+			#f2.write('*&E ')
+			tags.append('*&E')
 		if(line[1]==','):
-			f2.write('*&, ')
+			#f2.write('*&, ')
+			tags.append('*&,')
 		if(line[1]=='!'):
-			f2.write('*&! ')													
-		line = " ".join(line)
-		#print(line)
-		f1.write(line)
-		f1.write('\n')
+			#f2.write('*&! ')
+			tags.append('*&!')
 
-
-f4 = open("tweet.txt",'r')
-f2.seek(0,0)
-
-f5 = open("TaggedTweets.txt",'w')
-
-lines1 = f4.readlines()
-lines2 = f2.readlines()
-
-for i in range(len(lines1)):
-	line1 = lines1[i]
-	f5.write(line1)
-	line2 = lines2[i]
-	f5.write(line2)
-	f5.write('\n')
 
 
